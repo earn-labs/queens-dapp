225bb67f708f68d719358b26819d337d842b662a
trashpirate <nadinaoates@gmail.com>
2024-09-05 11:05:52 -0500

    clean up

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
-type Props = {};
 
-export default function Footer({ }: Props) {
+/*//////////////////////////////////////////////////////////////
+                          COMPONENT FOOTER
+//////////////////////////////////////////////////////////////*/
+
+export default function Footer() {
     return (
         <footer className="text-secondary text-center my-8 text-opacity-60 bottom-0 z-50 px-8 text-sm">
             <div>
-                Created by <Link className="font-bold" href="https://twitter.com/laylascreations">@laylascreations</Link> & <Link className="font-bold" href="https://twitter.com/N0_crypto">@N0_crypto</Link>
+                {"Created by "}
+                <Link className="font-bold" href="https://twitter.com/laylascreations">@laylascreations</Link>
+                {" & "}
+                <Link className="font-bold" href="https://twitter.com/0xTrashPirate">@0xTrashPirate</Link>
             </div>
-
         </footer>);
 }
